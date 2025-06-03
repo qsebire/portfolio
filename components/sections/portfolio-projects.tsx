75c6760e3bb002543dfe7cca8b313a03bd8077c5
Quentin <quentin.sebire@yahoo.com>
2025-06-03 17:51:28 +0200

    Remove filter

--- components/sections/portfolio-projects.tsx
+++ components/sections/portfolio-projects.tsx
@@ -166,18 +166,9 @@ const Project = ({
 };
 
 const PortfolioProjects = ({ projects }: { projects: projectType[] }) => {
-    const [filters, setFilters] = useState<string[]>([]);
     const [projectCurrentlyOpen, setProjectCurrentlyOpen] = useState<
         string | undefined
     >(undefined);
-
-    // function handleFilter(filterName: string) {
-    //     if (filters.includes(filterName)) {
-    //         setFilters(filters.filter((filter) => filter !== filterName));
-    //         return;
-    //     }
-    //     setFilters([...filters, filterName]);
-    // }
 
     // Get categories used in projects without duplicates
     const projectsCategories = [
@@ -198,56 +189,25 @@ const PortfolioProjects = ({ projects }: { projects: projectType[] }) => {
         return 0;
     });
 
-    // const filtersDisplay = projectsCategories.map((cat, id) => {
-    //     const isFiltered = filters.includes(cat.label);
-    //     return (
-    //         <button
-    //             onClick={() => handleFilter(cat.label)}
-    //             style={
-    //                 isFiltered
-    //                     ? {
-    //                           background: 'white',
-    //                           color: 'var(--background)',
-    //                       }
-    //                     : undefined
-    //             }
-    //             className='block py-[2px] px-4 rounded-full font-medium border hover:bg-white/20'
-    //             key={id}
-    //         >
-    //             {cat.label}
-    //         </button>
-    //     );
-    // });
-
     const displayedProjects = projects.map((project, index) => {
-        const isFiltered = project.catArr.some((cat) =>
-            filters.includes(cat.label)
+        return (
+            <div
+                className='lg:sticky lg:top-0'
+                style={{ zIndex: index }}
+                key={project.title}
+                onClick={() => setProjectCurrentlyOpen(project.title)}
+            >
+                <Project
+                    project={project}
+                    isOpen={projectCurrentlyOpen === project.title}
+                />
+            </div>
         );
-        if (isFiltered || filters.length === 0) {
-            return (
-                <div
-                    className='lg:sticky lg:top-0'
-                    style={{ zIndex: index }}
-                    key={project.title}
-                    onClick={() => setProjectCurrentlyOpen(project.title)}
-                >
-                    <Project
-                        project={project}
-                        isOpen={projectCurrentlyOpen === project.title}
-                    />
-                </div>
-            );
-        }
     });
 
     return (
         <div className='relative'>
-            {/* <div className='hidden lg:flex justify-center items-center gap-3 flex-wrap p-2 border-[0.5px] border-white sticky top-[calc(100vh-60px)] z-50 bg-background w-fit m-auto rounded-full'>
-                <p className='text-lg font-medium ml-4'>Filtres :</p>
-                {filtersDisplay}
-            </div> */}
             <div className=''>{displayedProjects}</div>
-            {/* <div className='relative h-[60px] z-[60] bg-background -mb-[60px]' /> */}
         </div>
     );
 };
